4f8aac156dbae5742e59951accd5ce1bf5164b06
David Snir <david.snir@gmail.com>
2026-04-16 16:24:17 +0300

    finished tasks class implamentations

--- models.py
+++ models.py
@@ -15,7 +15,8 @@ class Task:
         
     def to_json(self):
         return {
-            "task_id": str(self.id),
+            "task_id": str(self.task_id),
+            "parent_id": str(self.parent_id),
             "title": self.title,
             "is_complete": self.is_complete
         }
@@ -39,12 +40,39 @@ class Task:
                 return True
         return False
     
+    def edit_task(self, user_data: dict) -> Task:
+        """gets the task and a user data in dictionary
+        `{
+            "title": str,
+            "is_complete": bool
+        }` edits the task and returns a refrence for it (task)"""
+        for key, value in user_data.items():
+            setattr(self,key,value)
+        return self
+
+    def remove_task(self) -> None:
+        """remove task if it doesnt have sub task"""
+        if not self.task_id in Task.tasks_id_by_parent_id_list:
+            Task.tasks_by_task_id_list.pop(self.task_id)
+        else:
+            raise ValueError("Task have subtasks, the remove_task_and_sub_task function is needed")
+
+    def remove_task_and_sub_task(self) -> None:
+        """remove task and it's sub tasks recursivly"""
+        sub_tasks_id = Task.tasks_id_by_parent_id_list[self.task_id]
+        if sub_tasks_id:
+            for task_id in sub_tasks_id:
+                task: Task = Task.get_task_by_id(task_id)
+                task.remove_task_and_sub_task()
+        else:
+            self.remove_task()
+    
     @classmethod
     def is_task_exist(task_id: uuid.UUID) -> bool:
         return task_id in Task.tasks_by_task_id_list
     
     @classmethod
-    def get_class_by_id(task_id: uuid.UUID) -> Task:
+    def get_task_by_id(task_id: uuid.UUID) -> Task:
         if Task.is_task_exist(task_id):
             return Task.tasks_by_task_id_list[task_id]
         raise ValueError("Task does not exists")
@@ -58,26 +86,6 @@ class Task:
         new_task =Task(title=task_name,is_complete=is_complete)
         return new_task
     
-    @classmethod
-    def edit_task(task: Task, user_data: dict) -> Task:
-        """gets the task and a user data in dictionary
-        `{
-            "title": str,
-            "is_complete": bool
-        }` edits the task and returns a refrence for it (task)"""
-        for key, value in user_data.items():
-            setattr(task,key,value)
-        return task
-    
-    @classmethod
-    def remove_task(task: Task) -> None:
-        """remove task if it doesnt have sub task"""
-        if not task.task_id in Task.tasks_id_by_parent_id_list:
-            Task.tasks_by_task_id_list.pop(task.task_id)
-        else:
-            pass
-
-
     
 class Task_List:
     
@@ -90,7 +98,7 @@ class Task_List:
             self.tasks: list[Task] = []
             self._add_list(self)
         else:
-            raise ValueError("user name already exists")
+            raise ValueError("list name already exists")
             
     def is_exist(name: str):
         return name.lower() in Task_List.task_lists
@@ -98,55 +106,3 @@ class Task_List:
     def _generate_id(self) -> uuid:
         return uuid.uuid4()
     
-    
-    
-    
-tasks_list: list[Task] =[
-    Task("Learn Flask"),
-    Task("Build App"),
-    Task("Test With Postman",is_complete=True)
-]
-
-# def get_all_tasks() -> list[Task]:
-#     """returns a list of the tasks list"""
-#     global tasks_list
-#     result: list = []
-#     for task in tasks_list:
-#         result.append(task.to_json())
-#     return result
-
-# def get_task_by_id(task_id: str) -> Task:
-#     """return the task with same id as inputed if exists, if not return `None`"""
-#     result_task: Task = None
-    
-#     # searches fot the right task
-#     # TODO: change tasks_list var to dict for faster searching
-#     for task in tasks_list:
-#         if task_id == task.id:
-#             result_task = task
-#     return result_task
-
-# def create_task(task_name: str= None, is_complete: bool= None) -> Task:
-#     """get string name and status (optional) and adds the task to the task list. Returns the new_task cobject created"""
-#     if not(task_name or is_complete):
-#         new_task = Task()
-#     elif not task_name:
-#         new_task = Task(is_complete=is_complete)
-#     elif not is_complete:
-#         new_task = Task(task_name)
-        
-#     tasks_list.append(new_task)
-#     return new_task
-
-# def edit_task(task: Task, user_data: dict) -> None:
-#     """gets the task and a user data in dictionary
-#     `{
-#         "title": str,
-#         "is_complete": bool
-#     }` edits the task and returns a refrence for it (task)"""
-#     for key, value in user_data.items():
-#         setattr(task,key,value)
-#     return task
-
-def remove_task(task: Task) -> None:
-    tasks_list.remove(task)
